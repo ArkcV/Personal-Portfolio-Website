2c804929174ee9130125b746cd69ff7a25b073c3
ArkcV <andrielmarques@hotmail.com>
2024-11-21 19:46:01 -0300

    feat add skills html, css & js function

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,6 +31,23 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener("click", linkAction))
 
 
+/*==================== SKILLS ARROW OPEN Y CLOSE ====================*/
 
+const skillsContent = document.getElementsByClassName("skills_content")
+const skillsHeader = document.querySelectorAll(".skills_header")
 
+function toggleSkills(){
+  let itemClass = this.parentNode.className
+
+  for(i = 0; i < skillsContent.length; i++){
+    skillsContent[i].className = "skills_content skills_close"
+  }
+  if(itemClass === "skills_content skills_close"){
+    this.parentNode.className = "skills_content skills_open"
+  }
+}
+
+skillsHeader.forEach((el) =>{
+  el.addEventListener("click", toggleSkills)
+})
 
